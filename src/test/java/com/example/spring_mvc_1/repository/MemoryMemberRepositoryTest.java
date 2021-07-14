46503d1a6559efe3e5aa7c9e3a004392896611d4
rheew <47098896+rheew@users.noreply.github.com>
2021-07-14 10:10:15 +0900

    test class

---
+++ src/test/java/com/example/spring_mvc_1/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.example.spring_mvc_1.repository;
+
+import com.example.spring_mvc_1.domain.Member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("Spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+
+
+        assertThat(member).isEqualTo(result);
+
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+
+    }
+}

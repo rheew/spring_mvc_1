f0a18398f32b6994ac119b9a32dad539fa87c38c
rheew <47098896+rheew@users.noreply.github.com>
2021-07-19 16:57:05 +0900

    Spring AOP complete

--- src/main/java/com/example/spring_mvc_1/SpringConfig.java
+++ src/main/java/com/example/spring_mvc_1/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.example.spring_mvc_1;
 
+import com.example.spring_mvc_1.aop.TimeTraceAop;
 import com.example.spring_mvc_1.repository.*;
 import com.example.spring_mvc_1.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,11 @@ public class SpringConfig {
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository);
+    }
+
+    @Bean
+    public Object TimeTraceAop() {
+        return new TimeTraceAop();
     }
 
 //    @Bean
